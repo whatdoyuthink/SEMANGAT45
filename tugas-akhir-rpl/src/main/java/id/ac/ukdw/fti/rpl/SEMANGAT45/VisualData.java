e1450d702f1e6d3a07ef5bf02ca948d5fcc8c721
Yoel Christy Lumembang <80615307+whatdoyuthink@users.noreply.github.com>
2021-12-21 01:55:27 +0800

    Penambahan Visualisasi Data (Area Chart)

--- tugas-akhir-rpl/src/main/java/id/ac/ukdw/fti/rpl/SEMANGAT45/VisualData.java
+++ tugas-akhir-rpl/src/main/java/id/ac/ukdw/fti/rpl/SEMANGAT45/VisualData.java
@@ -5,19 +5,25 @@ import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import id.ac.ukdw.fti.rpl.SEMANGAT45.database.Database;
+import id.ac.ukdw.fti.rpl.SEMANGAT45.modal.Event;
+import id.ac.ukdw.fti.rpl.SEMANGAT45.modal.Places;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.AreaChart;
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.TitledPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
 public class VisualData implements Initializable {
+
+    Database db = new Database();
 
     @FXML
     private AnchorPane background;
@@ -52,7 +58,39 @@ public class VisualData implements Initializable {
     @FXML
     private Text JudulVisDat;
 
-    public void initialize(URL location, ResourceBundle resources){
+    private ObservableList<Places> places = FXCollections.observableArrayList();
+    private ObservableList<Event> events = FXCollections.observableArrayList();
+
+    public void initialize(URL location, ResourceBundle resourceBundle){
+        try {
+            places = Database.instance.visPlaces();
+            XYChart.Series dataresult = new XYChart.Series();
+            dataresult.setName("Places");
+            BentukVisDatPlaces.setLegendVisible(false);
+            for(int i = places.size()-1; i>-0; i--){
+                System.out.println(places.get(i).getDisplayTitle());
+                System.out.println(places.get(i).getEventsHere().split(",").length);                
+                dataresult.getData().add(new BarChart.Data(places.get(i).getDisplayTitle(),places.get(i).getEventsHere().split(",").length));
+            }
+            BentukVisDatPlaces.getData().add(dataresult);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        
+    /*try {
+            events = Database.instance.visEvent();
+            XYChart.Series dataresult = new XYChart.Series();
+            dataresult.setName("Event");
+            BentukVisDatEvent.setLegendVisible(false);
+            for(int i = places.size()-1; i>-0; i--){
+                System.out.println(events.get(i).getPlaces());
+                System.out.println(events.get(i).getPlaces().split(",").length);                
+                dataresult.getData().add(new BarChart.Data(events.get(i).getTitle(),events.get(i).getPlaces().split(",").length));
+            }
+            BentukVisDatEvent.getData().add(dataresult);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }*/
         
     }
 
--- tugas-akhir-rpl/src/main/java/id/ac/ukdw/fti/rpl/SEMANGAT45/database/Database.java
+++ tugas-akhir-rpl/src/main/java/id/ac/ukdw/fti/rpl/SEMANGAT45/database/Database.java
@@ -4,9 +4,15 @@ import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import id.ac.ukdw.fti.rpl.SEMANGAT45.modal.Verse;
 
+import id.ac.ukdw.fti.rpl.SEMANGAT45.modal.Event;
+import id.ac.ukdw.fti.rpl.SEMANGAT45.modal.Places;
+import id.ac.ukdw.fti.rpl.SEMANGAT45.modal.Verse;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -42,6 +48,45 @@ public class Database {
             System.out.println(e.getMessage());
         }
         return verses;
+
+    }
+        
+        ObservableList<Event>events = FXCollections.observableArrayList();
+        public ObservableList<Event> visEvent(){
+            String query = "SELECT verses, title, locations FROM events";
+            try {
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery(query);
+                while (result.next()){
+                    Event event = new Event();
+                    event.setVerses(result.getString("verses"));
+                    event.setTitle(result.getString("title"));
+                    event.setPlaces(result.getString("locations"));
+                    events.add(event);
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            return events;
+            
+        }
+        ObservableList<Places>places = FXCollections.observableArrayList();
+        public ObservableList<Places> visPlaces() {
+            String query = "SELECT verses, displayTitle, eventsHere FROM places";
+            try {
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery(query);
+                while(result.next()){
+                    Places place = new Places();
+                    place.setVerses(result.getString("verses"));
+                    place.setdisplayTitle(result.getString("displayTitle"));
+                    place.setEventsHere(result.getString("eventsHere"));
+                    places.add(place);
+                } 
+            } catch (Exception e) {
+                //TODO: handle exception
+            }
+            return places; 
     }
     public Connection opeConnection(Database database){
         return database.connection;
